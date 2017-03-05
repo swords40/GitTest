33df852dddb2fc55868acb2884279119eb66af7b
yjh <417575343@qq.com>
2017-03-05 15:45:27 +0800

    test github

---
+++ src/test/java/com/hpit/TestGit.java
@@ -0,0 +1,14 @@
+package com.hpit;
+
+import org.junit.Test;
+
+/**
+ * Created by yjh on 2017/3/5.
+ */
+public class TestGit {
+    @Test
+    public void test(){
+        System.out.println("hello git");
+        System.out.println("hello github");
+    }
+}
